aae5a3599dd14cc005be0b112929bb080f2a5470
Nicholas-Wolgamott <nwolgamott@gmail.com>
2022-02-19 10:58:37 -0500

    Updated forces with first lesson, adding the concept of applied forces as being equivalent to acceleration and their summation. Also how this affects the dynamics of a moving object.

--- Forces/mover.js
+++ Forces/mover.js
@@ -1,29 +1,53 @@
-// Mover with Random Vector, Mover.js
+// Forces, mover.js
 // From The Nature of Code by Dan Shiffman
 // Nicholas Wolgamott
 
 class Mover {
   constructor(x, y) {
     this.pos = createVector(x, y);
-    this.vel = p5.Vector.random2D()
-    this.vel.mult(random(3));
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.r = 16;
+    this.d = this.r * 2;
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  edges() {
+    if (this.pos.y + this.r >= height) {
+      this.pos.y = height - this.r;
+      this.vel.y *= -1;
+    }
+
+    /*if (this.pos.y <= 0) {
+      this.pos.y = 0;
+      this.vel.y *= -1;
+    }*/
+
+    if (this.pos.x + this.r >= width) {
+      this.pos.x = width - this.r;
+      this.vel.x *= -1;
+    }
+
+    if (this.pos.x <= this.r) {
+      this.pos.x = this.r;
+      this.vel.x *= -1;
+    }
   }
 
   update() {
-    let mouse = createVector(mouseX, mouseY);
-    this.acc = p5.Vector.sub(mouse, this.pos);
-    this.acc.setMag(1);
-
     this.vel.add(this.acc);
-    this.vel.limit(5);
-
     this.pos.add(this.vel);
+    this.edges();
+    this.acc.set(0, 0);
   }
 
   show() {
-    stroke(255, 100);
+    stroke(255);
     strokeWeight(2);
     fill(255, 100);
-    ellipse(this.pos.x, this.pos.y, 32);
+    ellipse(this.pos.x, this.pos.y, this.d);
   }
 }
--- Forces/sketch.js
+++ Forces/sketch.js
@@ -1,4 +1,4 @@
-// Mover with Random Vector. sketch.js
+// Forces, sketch.js
 // From The Nature of Code by Dan Shiffman
 // Nicholas Wolgamott
 
@@ -7,11 +7,19 @@ let mover;
 function setup() {
 	createCanvas(400, 400);
 	mover = new Mover(200, 200);
-	// background(0);
 }
 
 function draw() {
 	background(0);
+
+	let gravity = createVector(0, 0.2);
+	mover.applyForce(gravity);
+
+	if (mouseIsPressed) {
+		let wind = createVector(0.1, 0);
+		mover.applyForce(wind);
+	}
+
 	mover.update();
 	mover.show();
 }
