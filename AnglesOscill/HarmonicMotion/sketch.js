59c19984dcec19e0b3395a5027437c90b2b7b5cd
Nicholas-Wolgamott <nwolgamott@gmail.com>
2022-03-02 01:42:12 -0500

    Added an oscillating series of circles to follow a sin wave like motion

--- AnglesOscill/HarmonicMotion/sketch.js
+++ AnglesOscill/HarmonicMotion/sketch.js
@@ -4,24 +4,36 @@
 
 /// <reference path="c:\\Users\\monke\\.vscode\\extensions\\samplavigne.p5-vscode-1.2.8\\p5types\\global.d.ts" />
 
-let angle = 0;
-let angleV = 0;
+let angles = [];
+let angleVs = [];
+let r = 8;
+
 
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(600, 400);
+	let total = floor(width / (r * 2));
+	for (let i = 0; i < total + 1; i++) {
+		angles[i] = map(i, 0, total, 0, 2 * TWO_PI);
+		angleVs[i] = 0.02;
+	}
 }
 
 function draw() {
 	background(0);
-	translate(200, 200);
-	fill(252, 238, 33);
-	//let r = map(sin(angle), -1, 1, 0, 200);
-	let y = map(sin(angle), -1, 1, -200, 200);
-	stroke(255);
-	line(0, 0, 0, y);
-	circle(0, y, 32);
-
-	angle += angleV;
-	angleV += 0.0001;
-
+	translate(300, 200);
+	stroke(252, 238, 33);
+	beginShape();
+	for (let i = 0; i < angles.length; i++) {
+		let y = map(sin(angles[i]), -1, 1, -200, 200);
+		let x = map(i, 0, angles.length, -300, 300);
+		strokeWeight(4);
+		// line(x, 0, x, y);
+		fill(252, 238, 33);
+		circle(x, y, r * 2);
+		noFill();
+		vertex(x, y);
+		angles[i] += angleVs[i];
+		// angleVs[i] += 0.0001;
+	}
+	endShape();
 }
