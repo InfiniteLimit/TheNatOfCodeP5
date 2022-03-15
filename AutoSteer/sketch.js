ff8b0b7dd746118b8f94904e996d80754c96c8ea
Nicholas-Wolgamott <nwolgamott@gmail.com>
2022-03-14 20:53:53 -0400

    Added the wander behavior to vehicle.

--- AutoSteer/sketch.js
+++ AutoSteer/sketch.js
@@ -7,21 +7,16 @@
 let vehicle;
 
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(800, 600);
 	vehicle = new Vehicle(100, 100);
 }
 
 function draw() {
 	background(0);
 
-	let target = createVector(mouseX, mouseY);
-	fill(255, 0, 0);
-	noStroke();
-	ellipse(target.x, target.y, 32);
+	vehicle.wander();
 
-	let steering = vehicle.arrive(target);
-	vehicle.applyForce(steering);
 	vehicle.update();
 	vehicle.show();
-
+	vehicle.edges();
 }
--- AutoSteer/vehicle.js
+++ AutoSteer/vehicle.js
@@ -7,11 +7,36 @@
 class Vehicle {
   constructor(x, y) {
     this.pos = createVector(x, y);
-    this.vel = createVector(0, 0);
+    this.vel = createVector(1, 0);
     this.acc = createVector(0, 0);
     this.r = 16;
-    this.maxSpeed = 6;
-    this.maxForce = 0.4;
+    this.maxSpeed = 4;
+    this.maxForce = 0.2;
+    this.wanderTheta = PI / 2;
+
+    this.currentPath = [];
+    this.paths = [this.currentPath];
+  }
+
+  wander() {
+    let wanderPoint = this.vel.copy();
+    wanderPoint.setMag(100);
+    wanderPoint.add(this.pos);
+
+    let wanderRadius = 50;
+
+    let theta = this.wanderTheta + this.vel.heading();
+
+    let x = wanderRadius * cos(theta);
+    let y = wanderRadius * sin(theta);
+    wanderPoint.add(x, y);
+
+    let steer = wanderPoint.sub(this.pos);
+    steer.limit(this.maxForce);
+    this.applyForce(steer);
+
+    let displaceRange = 0.3;
+    this.wanderTheta += random(-displaceRange, displaceRange);
   }
 
   arrive(target) {
@@ -66,19 +91,33 @@ class Vehicle {
     this.vel.limit(this.maxSpeed);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
+
+    this.currentPath.push(this.pos.copy());
   }
 
   edges() {
+    let hitEdge = false;
+
     if (this.pos.y > height + this.r) {
       this.pos.y = -this.r;
+      hitEdge = true;
     } else if (this.pos.y < -this.r) {
       this.pos.y = height + this.r;
+      hitEdge = true;
     }
 
     if (this.pos.x > width + this.r) {
       this.pos.x = -this.r;
+      hitEdge = true;
     } else if (this.pos.x < -this.r) {
       this.pos.x = width + this.r;
+      hitEdge = true;
+    }
+
+    if (hitEdge) {
+      this.currentPath = [];
+      this.paths.push(this.currentPath);
+
     }
   }
 
@@ -91,6 +130,15 @@ class Vehicle {
     rotate(this.vel.heading());
     triangle(-this.r, -this.r / 2, -this.r, this.r / 2, this.r, 0);
     pop();
+
+    for (let path of this.paths) {
+      noFill();
+      beginShape();
+      for (let v of path) {
+        vertex(v.x, v.y);
+      }
+      endShape();
+    }
   }
 }
 
